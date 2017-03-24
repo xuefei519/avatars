9ce36e27ef7f020204f4b8b90479d3361da3c535
xshi <xuefei.shi@mail.mcgill.ca>
2017-03-24 14:27:25 -0400

    addded Tsing

---
+++ src/Tsing.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './style/Tsing.css';
+
+class Tsing extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="profile">
+          <div className="group">
+            <div className="cap"></div>
+            <div className="hair"></div>
+            <div className="ear"></div>
+            <div className="ear right"></div>
+            <div className="face"></div>
+            <div className="eyebrow"></div>
+            <div className="eye"></div>
+            <div className="eye right"></div>
+            <div className="glasses"></div>
+            <div className="blush"></div>
+            <div className="mouth">
+            </div>
+            <div className="neck"></div>
+            <div className="body">
+              <div className="collar">
+              </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default Tsing;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Xuefei from './Xuefei';
+import Tsing from './Tsing';
 import Christina from './Christina';
 import './style/index.css';
 
 ReactDOM.render(
   <Xuefei />,
   document.getElementById('Xuefei')
+);
+ReactDOM.render(
+  <Tsing />,
+  document.getElementById('Tsing')
 );
 ReactDOM.render(
   <Christina />,
