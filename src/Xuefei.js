c868654f2c5c81ef7cf0802ec3ddd60419d72041
xshi <xuefei.shi@mail.mcgill.ca>
2018-10-16 17:18:24 -0400

    added bridal look for Xuefei

--- src/Xuefei.js
+++ src/Xuefei.js
@@ -7,12 +7,19 @@ class Xuefei extends Component {
       <a className="container">
         <div className="profile">
           <div className="group">
+            <div className='deco'>
+              <div className="earring"></div>
+              <div className="eyebrow"></div>
+              <div className="hair"></div>
+            </div>
+            <div className="tohide">
+            <div className="eyebrow"></div>
             <div className="hair"></div>
+            </div>
             <div className="ear"></div>
             <div className="ear right"></div>
             <div className="fringe"></div>
             <div className="face"></div>
-            <div className="eyebrow"></div>
               <div className="eye"></div>
               <div className="eye right"></div>
               <div className="glasses"></div>
@@ -21,9 +28,18 @@ class Xuefei extends Component {
             <div className="blush"></div>
             <div className="nose"></div>
             <div className="neck"></div>
-            <div className="body">
-            <div className="collar"></div>
-            </div>
+              <div className="tohide">
+                <div className="body">
+                  <div className="collar"></div>
+                </div>
+              </div>
+              <div className="deco">
+                <div className="body">
+                  <div className="collar"></div>
+                  <div className="necklace"></div>
+                  <div className="dress"></div>
+                </div>
+              </div>
           </div>
         </div>
         <p className="name">Xuefei</p>
