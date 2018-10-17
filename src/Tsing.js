6761b1069e7b16cd51e6e20101672b4ff35023f4
xshi <xuefei.shi@mail.mcgill.ca>
2018-10-17 10:44:54 -0400

    added groom look for Tsing

--- src/Tsing.js
+++ src/Tsing.js
@@ -8,31 +8,42 @@ class Tsing extends Component {
         <div className="profile">
           <div className="group">
 
-            <div className="hair"></div>
+
             <div className="ear"></div>
             <div className="ear right"></div>
             <div className="face"></div>
             <div className="eyebrow"></div>
             <div className="tohide">
               <div className="cap"></div>
-              <div className="eye"></div>
-              <div className="eye right"></div>
+              <div className="hair"></div>
+              <div className="dummyEye"></div>
+              <div className="dummyEye right"></div>
               <div className="glasses"></div>
               <div className="blush"></div>
               <div className="mouth"></div>
-            </div>
-            <div className="deco">
-              <div className="dummyEye"></div>
-              <div className="dummyEye right"></div>
-              <div className="dummyBlush"></div>
-              <div className="dummyMouth"></div>
+
+              <div className="neck"></div>
+              <div className="body">
+                <div className="collar">
+                </div>
             </div>
 
-            <div className="neck"></div>
-            <div className="body">
-              <div className="collar">
-              </div>
-          </div>
+            </div>
+            <div className="deco">
+              <div className="eye"></div>
+              <div className="eye right"></div>
+              <div className="dummyBlush"></div>
+              <div className="dummyMouth"></div>
+              <div className="hair"></div>
+              <div className="fringe"></div>
+
+              <div className="neck"></div>
+              <div className="bowtie"></div>
+              <div className="body">
+                <div className="collar"></div>
+                </div>
+            </div>
+
         </div>
       </div>
       <p className='name'>Tsing</p>
